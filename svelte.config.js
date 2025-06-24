d925b7005a7f03f324b6fc5c1899649c9e8105d1
Wolfe-Jam <wolfejam2020@gmail.com>
2025-06-24 12:48:02 -0400

    Add Netlify SPA configuration and fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,6 @@
-import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  kit: {
-    adapter: adapter()
-  },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
